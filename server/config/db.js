1816069ffd8776fe52451c3bc593272382186629
Saidev18 <chakkasaidev18@gmail.com>
2026-07-27 20:35:04 +0530

    Configure Docker, Kubernetes and Jenkins pipeline

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb://mongodb:27017/Ecommerce", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
